2a2dc3752bd318ee0d3635411e98f03e4e989870
Marti Congost <marti.congost@skyscanner.net>
2024-12-09 20:46:02 +0100

    Improve the layout of actions

--- src/components/ResourceSetDisplay/ResourceSetDisplay.tsx
+++ src/components/ResourceSetDisplay/ResourceSetDisplay.tsx
@@ -2,8 +2,6 @@ import { resources, ResourceSet } from "@/models/resources"
 import styles from "./ResourceSetDisplay.module.scss"
 import { createElement } from "react"
 import { getStandardAttributes, type StandardComponentProps } from "@/modules/react-utils"
-import NoneIcon from "@/svg/none.svg"
-import VariableIcon from "@/svg/variable.svg"
 
 export interface ResourceSetDisplayProps extends StandardComponentProps {
   resourceSet?: ResourceSet
@@ -22,21 +20,19 @@ export const ResourceSetDisplay = ({
     data-arrangement={arrangement}
     data-background={background}
   >
-    {resourceSet === undefined ? (
-      <VariableIcon className={styles.variableIcon} />
-    ) : resourceSet.isNone() ? (
-      <NoneIcon className={styles.noneIcon} />
-    ) : (
-      Object.values(resources).map((resource) => {
-        const amount = resourceSet[resource.id]
-        if (!amount) return null
-        return (
-          <div className={styles.resource} data-resource={resource.id}>
-            {createElement(resource.icon, { className: styles.icon })}
-            <div className={styles.amount}>{amount}</div>
-          </div>
-        )
-      })
-    )}
+    {resourceSet === undefined
+      ? "Variable"
+      : resourceSet.isNone()
+        ? "Gratis"
+        : Object.values(resources).map((resource) => {
+            const amount = resourceSet[resource.id]
+            if (!amount) return null
+            return (
+              <div className={styles.resource} data-resource={resource.id}>
+                {createElement(resource.icon, { className: styles.icon })}
+                <div className={styles.amount}>{amount}</div>
+              </div>
+            )
+          })}
   </div>
 )
