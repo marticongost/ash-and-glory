15b48b6477b3fc292f1e3c61845b408bb2ee1144
mcongost <mcongost@guellconsulting.com>
2024-12-14 19:47:04 +0100

    Complete the map distributions for higher player counts

--- src/content/rules/Preparation/Preparation.tsx
+++ src/content/rules/Preparation/Preparation.tsx
@@ -18,56 +18,85 @@ export const Preparation = ({ ...baseProps }: PreparationProps) => (
       jugador, de manera que tots els jugadors puguin veure-les. Les cartes no utilitzades es retiren del joc.
     </Section>
     <Section title="3. Preparar el mapa">
-      <MapDistribution
-        playerCount={2}
-        mapSize={4}
-        startPositions={[
-          [0, 3],
-          [0, -3],
-        ]}
-      />
-      <MapDistribution
-        playerCount={3}
-        mapSize={5}
-        startPositions={[
-          [0, -4],
-          [4, 2],
-          [-4, 2],
-        ]}
-      />
-      <MapDistribution
-        playerCount={4}
-        mapSize={6}
-        startPositions={[
-          [-4, 3],
-          [-3, -4],
-          [3, 3],
-          [4, -3],
-        ]}
-      />
-      <MapDistribution
-        playerCount={5}
-        mapSize={7}
-        startPositions={[
-          [-4, -4],
-          [4, -4],
-          [-6, 2],
-          [6, 2],
-          [0, 6],
-        ]}
-      />
-      <MapDistribution
-        playerCount={6}
-        mapSize={8}
-        startPositions={[
-          [0, 7],
-          [0, -7],
-          [-4, -4],
-          [7, -4],
-          [7, 3],
-          [-7, 3],
-        ]}
-      />
+      <div className={styles.mapDistributions}>
+        <MapDistribution
+          playerCount={2}
+          mapSize={4}
+          startPositions={[
+            [0, 3],
+            [0, -3],
+          ]}
+        />
+        <MapDistribution
+          playerCount={3}
+          mapSize={5}
+          startPositions={[
+            [0, -4],
+            [4, 2],
+            [-4, 2],
+          ]}
+        />
+        <MapDistribution
+          playerCount={4}
+          mapSize={6}
+          startPositions={[
+            [-4, 3],
+            [-3, -4],
+            [3, 3],
+            [4, -3],
+          ]}
+        />
+        <MapDistribution
+          playerCount={5}
+          mapSize={7}
+          startPositions={[
+            [-4, -4],
+            [4, -4],
+            [-6, 2],
+            [6, 2],
+            [0, 6],
+          ]}
+        />
+        <MapDistribution
+          playerCount={6}
+          mapSize={8}
+          startPositions={[
+            [0, 7],
+            [0, -7],
+            [-7, -4],
+            [7, -4],
+            [7, 3],
+            [-7, 3],
+          ]}
+        />
+        <MapDistribution
+          playerCount={7}
+          mapSize={9}
+          startPositions={[
+            [-8, 4],
+            [-8, -3],
+            [-2, -7],
+            [5, -6],
+            [8, 0],
+            [5, 5],
+            [-2, 7],
+          ]}
+        />
+        <MapDistribution
+          playerCount={8}
+          mapSize={9}
+          startPositions={[
+            [0, -8],
+            [6, -5],
+            [8, 0],
+            [6, 5],
+            [0, 8],
+            [-6, 5],
+            [-8, 0],
+            [-6, -5],
+          ]}
+        />
+      </div>
     </Section>
   </Section>
 )
@@ -79,7 +108,7 @@ interface MapDistributionProps {
 }
 
 const MapDistribution = ({ playerCount, mapSize, startPositions }: MapDistributionProps) => (
-  <Section title={`Distribució per ${playerCount} jugadors`}>
+  <Section title={`Distribució per ${playerCount} jugadors`} className={styles.mapDistributionSection}>
     <HexGrid
       className={styles.mapDistributionHexGrid}
       size={mapSize}
