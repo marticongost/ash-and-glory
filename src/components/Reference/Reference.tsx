1e4951fa4724a03e228a937fd41d8f1831e3dd2c
Marti Congost <marti.congost@skyscanner.net>
2025-01-01 15:35:41 +0100

    Support lazy referenced items in the Reference component

--- src/components/Reference/Reference.tsx
+++ src/components/Reference/Reference.tsx
@@ -3,7 +3,7 @@ import styles from "./Reference.module.scss"
 import { getStandardAttributes, type StandardComponentProps } from "@/modules/react-utils"
 
 export interface ReferenceProps extends StandardComponentProps {
-  item: ReferencedItem
+  item: ReferencedItem | (() => ReferencedItem)
 }
 
 export interface ReferencedItem {
@@ -12,9 +12,12 @@ export interface ReferencedItem {
   icon: JSXElementConstructor<any>
 }
 
-export const Reference = ({ item, ...baseProps }: ReferenceProps) => (
-  <span {...getStandardAttributes(baseProps, styles.Reference)} data-item={item.id}>
-    {createElement(item.icon, { className: styles.icon })}
-    <span className={styles.title}>{item.title}</span>
-  </span>
-)
+export const Reference = ({ item, ...baseProps }: ReferenceProps) => {
+  item = item instanceof Function ? item() : item
+  return (
+    <span {...getStandardAttributes(baseProps, styles.Reference)} data-item={item.id}>
+      {createElement(item.icon, { className: styles.icon })}
+      <span className={styles.title}>{item.title}</span>
+    </span>
+  )
+}
