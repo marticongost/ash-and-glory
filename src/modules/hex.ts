c0638e08ec2a831755eb86ca5fcacad1b670accc
mcongost <mcongost@guellconsulting.com>
2025-03-08 12:13:25 +0100

    Support subclasses of Hex

--- src/modules/hex.ts
+++ src/modules/hex.ts
@@ -1,4 +1,4 @@
-import { repeat } from "./utils"
+import { createSibling, repeat } from "./utils"
 
 interface OffsetCoordinates {
   column: number
@@ -13,7 +13,12 @@ interface CubeCoordinates {
 
 export interface HexProps extends Partial<OffsetCoordinates> {}
 
-export class Hex {
+export type HexExtraProps<P extends HexProps> = Omit<P, "column" | "row">
+export type HexInitializer<P extends HexProps> =
+  | HexExtraProps<P>
+  | ((coordinates: OffsetCoordinates) => HexExtraProps<P>)
+
+export class Hex<P extends HexProps = HexProps> {
   readonly column: number
   readonly row: number
 
@@ -24,6 +29,14 @@ export class Hex {
 
   static at(column: number, row: number) {
     return new this({ column, row })
+  }
+
+  private createHex(coordinates: OffsetCoordinates, initializer: HexInitializer<P>): this {
+    return createSibling(this, {
+      column: coordinates.column,
+      row: coordinates.row,
+      ...(initializer instanceof Function ? initializer(coordinates) : initializer),
+    })
   }
 
   toString(): string {
@@ -39,75 +52,99 @@ export class Hex {
     return Math.max(Math.abs(vector.q), Math.abs(vector.r), Math.abs(vector.s))
   }
 
-  north(n: number = 1): Hex {
-    return new Hex({ column: this.column, row: this.row - n })
+  north(initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.northTimes(1, initializer)
   }
 
-  south(n: number = 1): Hex {
-    return new Hex({ column: this.column, row: this.row + n })
+  northTimes(n: number = 1, initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.createHex({ column: this.column, row: this.row - n }, initializer)
   }
 
-  northWest(n: number = 1): Hex {
+  south(initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.southTimes(1, initializer)
+  }
+
+  southTimes(n: number = 1, initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.createHex({ column: this.column, row: this.row + n }, initializer)
+  }
+
+  northWest(initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.northWestTimes(1, initializer)
+  }
+
+  northWestTimes(n: number = 1, initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
     let { column, row } = this
     for (let i = 0; i < n; i++) {
       column -= 1
       row += column % 2 ? -1 : 0
     }
-    return new Hex({ column, row })
+    return this.createHex({ column, row }, initializer)
   }
 
-  southWest(n: number = 1): Hex {
+  southWest(initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.southWestTimes(1, initializer)
+  }
+
+  southWestTimes(n: number = 1, initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
     let { column, row } = this
     for (let i = 0; i < n; i++) {
       column -= 1
       row += column % 2 ? 0 : 1
     }
-    return new Hex({ column, row })
+    return this.createHex({ column, row }, initializer)
   }
 
-  northEast(n: number = 1): Hex {
+  northEast(initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.northEastTimes(1, initializer)
+  }
+
+  northEastTimes(n: number = 1, initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
     let { column, row } = this
     for (let i = 0; i < n; i++) {
       column += 1
       row += column % 2 ? -1 : 0
     }
-    return new Hex({ column, row })
+    return this.createHex({ column, row }, initializer)
   }
 
-  southEast(n: number = 1): Hex {
+  southEast(initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
+    return this.southEastTimes(1, initializer)
+  }
+
+  southEastTimes(n: number = 1, initializer: HexInitializer<P> = {} as HexInitializer<P>): this {
     let { column, row } = this
     for (let i = 0; i < n; i++) {
       column += 1
       row += column % 2 ? 0 : 1
     }
-    return new Hex({ column, row })
+    return this.createHex({ column, row }, initializer)
   }
 
-  outwardRing(distance: number): Hex[] {
+  outwardRing(distance: number, initializer: HexInitializer<P> = {} as HexInitializer<P>): this[] {
     if (distance < 0) {
       throw new Error(`Invalid circle size ${distance}; expected a number >= than 0`)
     }
     if (distance === 0) {
       return [this]
     }
-    const circle: Hex[] = []
-    let cursor = this.north(distance)
-    circle.push(...repeat(distance, () => (cursor = cursor.southEast())))
-    circle.push(...repeat(distance, () => (cursor = cursor.south())))
-    circle.push(...repeat(distance, () => (cursor = cursor.southWest())))
-    circle.push(...repeat(distance, () => (cursor = cursor.northWest())))
-    circle.push(...repeat(distance, () => (cursor = cursor.north())))
-    circle.push(...repeat(distance, () => (cursor = cursor.northEast())))
+    const circle: this[] = []
+    let cursor = this.northTimes(distance, initializer)
+    circle.push(...repeat(distance, () => (cursor = cursor.southEast(initializer))))
+    circle.push(...repeat(distance, () => (cursor = cursor.south(initializer))))
+    circle.push(...repeat(distance, () => (cursor = cursor.southWest(initializer))))
+    circle.push(...repeat(distance, () => (cursor = cursor.northWest(initializer))))
+    circle.push(...repeat(distance, () => (cursor = cursor.north(initializer))))
+    circle.push(...repeat(distance, () => (cursor = cursor.northEast(initializer))))
     return circle
   }
 
-  concentricRings(distance: number): Hex[] {
+  concentricRings(distance: number, initializer: HexInitializer<P> = {} as HexInitializer<P>): this[] {
     if (distance < 0) {
       throw new Error(`Invalid area size ${distance}; expected a number >= than 0`)
     }
-    const area: Hex[] = [this]
+    const area: this[] = [this]
     for (let i = 1; i <= distance; i++) {
-      area.push(...this.outwardRing(i))
+      area.push(...this.outwardRing(i, initializer))
     }
     return area
   }
@@ -126,48 +163,48 @@ const subtractCubeCoordinates = (a: CubeCoordinates, b: CubeCoordinates): CubeCo
   s: a.s - b.s,
 })
 
-export class HexSetBuilder {
-  private _hexes: Hex[]
-  private _cursor: Hex
+export class HexSetBuilder<H extends Hex = Hex> {
+  private _hexes: H[]
+  private _cursor: H
 
-  constructor() {
-    this._hexes = []
-    this._cursor = new Hex()
+  constructor(center: H) {
+    this._hexes = [center]
+    this._cursor = center
   }
 
-  push(hexMovement: (cursor: Hex) => Hex): HexSetBuilder {
+  push(hexMovement: (cursor: H) => H): this {
     this._cursor = hexMovement(this._cursor)
     this._hexes.push(this._cursor)
     return this
   }
 
-  add(hexMovement: (cursor: Hex) => Hex): HexSetBuilder {
+  add(hexMovement: (cursor: H) => H): this {
     this._hexes.push(hexMovement(this._cursor))
     return this
   }
 
-  skip(hexMovement: (cursor: Hex) => Hex): HexSetBuilder {
+  skip(hexMovement: (cursor: H) => H): this {
     this._cursor = hexMovement(this._cursor)
     return this
   }
 
-  build(): HexSet {
+  build(): HexSet<H> {
     return new HexSet(this._hexes)
   }
 }
 
-export class HexSet {
-  readonly hexes: Hex[]
+export class HexSet<H extends Hex = Hex> {
+  readonly hexes: H[]
   readonly top: number
   readonly bottom: number
   readonly left: number
   readonly right: number
 
   static builder(): HexSetBuilder {
-    return new HexSetBuilder().push((center) => center)
+    return new HexSetBuilder(new Hex())
   }
 
-  constructor(hexes: Hex[]) {
+  constructor(hexes: H[]) {
     this.hexes = []
 
     this.top = 0
