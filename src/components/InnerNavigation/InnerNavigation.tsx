5e1b6a52c0ded834c1ac293174446889348e408d
mcongost <mcongost@guellconsulting.com>
2025-03-15 15:00:23 +0100

    Add UI for nested sections

---
+++ src/components/InnerNavigation/InnerNavigation.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import styles from "./InnerNavigation.module.scss"
+import { getStandardAttributes, type StandardComponentProps } from "@/modules/react-utils"
+import { requireTopSection } from "@/modules/navigation"
+import Link from "next/link"
+import { createElement } from "react"
+
+export interface InnerNavigationProps extends StandardComponentProps {}
+
+export const InnerNavigation = ({ ...baseProps }: InnerNavigationProps) => {
+  const pathName = usePathname()
+  const topSection = requireTopSection(pathName)
+  return topSection.children.length ? (
+    <nav {...getStandardAttributes(baseProps, styles.InnerNavigation)}>
+      {topSection.children.map((section) => (
+        <Link
+          className={styles.link}
+          key={section.id}
+          href={section.url}
+          data-state={section.getSelectionState(pathName)}
+        >
+          {createElement(section.icon, { className: styles.icon })}
+          <span className={styles.label}>{section.title}</span>
+        </Link>
+      ))}
+    </nav>
+  ) : null
+}
---
+++ src/components/InnerNavigation/index.tsx
@@ -0,0 +1,1 @@
+export { InnerNavigation } from "./InnerNavigation"
